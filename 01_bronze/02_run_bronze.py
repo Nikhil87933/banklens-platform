1ac86f8d36fe5fdadfc4744be2e7bc37e3c3e882
nikhil_chamle <chamlenikhil1@gmail.com>
2026-06-09 20:19:25 +0000

    feat: add bronze batch runner

--- 01_bronze/02_run_bronze.py
+++ 01_bronze/02_run_bronze.py
@@ -1,17 +1,26 @@
-import uuid
-
 from importlib import import_module
+import uuid
 
 framework = import_module(
     "01_bronze.01_bronze_ingestion_framework"
+)
+
+config = import_module(
+    "00_setup.01_config"
 )
 
 batch_id = str(
     uuid.uuid4()
 )
 
-framework.ingest_table(
-    "customer_master",
-    1,
-    batch_id
+for table_name in config.TABLE_NAMES:
+
+    framework.ingest_table(
+        table_name,
+        1,
+        batch_id
+    )
+
+print(
+    "Bronze load complete"
 )

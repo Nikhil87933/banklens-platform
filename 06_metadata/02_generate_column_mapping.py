3b807b1e92fd03cf3a3205aa3ca82276e474ebc6
nikhil_chamle <chamlenikhil1@gmail.com>
2026-06-11 10:18:35 +0000

    adding generate column mapping

---
+++ 06_metadata/02_generate_column_mapping.py
@@ -0,0 +1,64 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+
+TABLES = [
+
+    "merchant_reference",
+    "market_rates",
+
+    "customer_master",
+    "account_master",
+    "loan_master",
+    "product_holdings",
+
+    "transaction_fact",
+    "card_transaction_fact",
+    "account_balance_snapshot",
+
+    "device_events",
+    "digital_activity",
+    "support_tickets"
+]
+
+rows = []
+
+for table_name in TABLES:
+
+    print(
+        f"Scanning {table_name}"
+    )
+
+    df = spark.table(
+        f"banklens.bronze.brz_{table_name}"
+    )
+
+    for column_name in df.columns:
+
+        if column_name.startswith("_"):
+            continue
+
+        rows.append(
+            (
+                table_name,
+                column_name,
+                "STRING",
+                "",
+                True
+            )
+        )
+
+metadata_df = spark.createDataFrame(
+    rows,
+    [
+        "table_name",
+        "column_name",
+        "target_type",
+        "format_string",
+        "is_active"
+    ]
+)
+
+display(
+    metadata_df
+)

42ceaaa0c72c8bf62a016f36d47d255ec4e908cf
nikhil_chamle <chamlenikhil1@gmail.com>
2026-06-11 06:14:09 +0000

    complete silver operational framework

--- 02_silver/02_run_silver.py
+++ 02_silver/02_run_silver.py
@@ -14,7 +14,22 @@ print(
 )
 
 TABLES = [
-    "merchant_reference"
+
+    "merchant_reference",
+    "market_rates",
+
+    "customer_master",
+    "account_master",
+    "loan_master",
+    "product_holdings",
+
+    "transaction_fact",
+    "card_transaction_fact",
+    "account_balance_snapshot",
+
+    "device_events",
+    "digital_activity",
+    "support_tickets"
 ]
 
 for table_name in TABLES:

bd93a07e2d611ddd8e5d496ae574178ae2d6f39e
nikhil_chamle <chamlenikhil1@gmail.com>
2026-06-22 10:06:53 +0000

    add incremental bronze loading

--- 01_bronze/02_run_bronze.py
+++ 01_bronze/02_run_bronze.py
@@ -9,15 +9,32 @@ config = import_module(
     "00_setup.01_config"
 )
 
+audit_utils = import_module(
+    "99_utils.audit_utils"
+)
+
 batch_id = str(
     uuid.uuid4()
 )
 
 for table_name in config.TABLE_NAMES:
 
+    last_day = (
+        audit_utils.get_last_successful_day(
+            table_name,
+            "BRONZE"
+        )
+    )
+
+    next_day = last_day + 1
+
+    print(
+        f"{table_name} -> Loading Day {next_day}"
+    )
+
     framework.ingest_table(
         table_name,
-        1,
+        next_day,
         batch_id
     )
 
